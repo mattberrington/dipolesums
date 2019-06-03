d7c2b9bfd9f34f233fac0cd6f2986165e3db49af
matthewberrington <berrington95@gmail.com>
2019-06-03 11:25:35 +1000

    Cleaning up DyCl3 example more

--- DyCl3/DipoleGroundStatev2.py
+++ DyCl3/DipoleGroundStatev2.py
@@ -14,52 +14,53 @@ def Rz(theta):
 
 class MonoclinicLattice:
 	def __init__(self):
-		self.Position = {}
+		self.position = {}
 
-	def Axes(self,a_len,b_len,c_len,beta):
+	def axes(self,a_len,b_len,c_len,beta):
 		""" Define the crystalographic properties of the lattice"""
 		self.a = np.array([0,0,a_len])
 		self.b = np.array([0,b_len,0])
 		self.c = Ry(-beta) @ np.array([0,0,c_len])
 
-	def gTensor(self,gx,gy,gz,zeta_a):
+	def g_tensor(self,gx,gy,gz,zeta_a):
 		""" Define the g-tensor of the crystal.
 		Assumes a axial tensor in the AC plane
 		"""
 		self.gx = gx	
 		self.gy = gy
 		self.gz = gz
+		self.g_grid = np.array([[gx*gx, gx*gy, gx*gz],[gy*gx, gy*gy, gy*gz],[gz*gx, gz*gy, gz*gz]])
 		# rotate the crystal coordinates so that I'm now in the coordinate system 
 		# given by the zeeman tensor's principal axes
 		self.a = Ry(zeta_a) @ self.a
 		self.b = Ry(zeta_a) @ self.b
 		self.c = Ry(zeta_a) @ self.c
 
-	def Ion1Position(self,x,y,z):
+	def ion1_position(self,x,y,z):
 		#Note Ive swapped a and c to match the niemiejer paper
 		self.ion1 = x*self.a + y*self.b + z*self.c
-		self.Position['1A'] = np.array([0,0,0]) + self.ion1
-		self.Position['2A'] = self.c + self.ion1
-		self.Position['3A'] = self.b + self.ion1
-		self.Position['4A'] = self.c + self.b + self.ion1
-		self.Position['5A'] = self.a + self.ion1
-		self.Position['6A'] = self.a + self.c + self.ion1
-		self.Position['7A'] = self.b + self.a + self.ion1
-		self.Position['8A'] = self.a + self.b + self.c + self.ion1
+		self.position['1A'] = np.array([0,0,0]) + self.ion1
+		self.position['2A'] = self.c + self.ion1
+		self.position['3A'] = self.b + self.ion1
+		self.position['4A'] = self.c + self.b + self.ion1
+		self.position['5A'] = self.a + self.ion1
+		self.position['6A'] = self.a + self.c + self.ion1
+		self.position['7A'] = self.b + self.a + self.ion1
+		self.position['8A'] = self.a + self.b + self.c + self.ion1
 
-	def Ion2Position(self,x,y,z):
+	def ion2_position(self,x,y,z):
 		self.ion2 = x*self.a + y*self.b + z*self.c
-		self.Position['1B'] = np.array([0,0,0]) + self.ion2
-		self.Position['2B'] = self.a + self.ion2
-		self.Position['3B'] = self.b + self.ion2
-		self.Position['4B'] = self.a + self.b + self.ion2
-		self.Position['5B'] = self.c + self.ion2
-		self.Position['6B'] = self.a + self.c + self.ion2
-		self.Position['7B'] = self.b + self.c + self.ion2
-		self.Position['8B'] = self.a + self.b + self.c + self.ion2
+		self.position['1B'] = np.array([0,0,0]) + self.ion2
+		self.position['2B'] = self.a + self.ion2
+		self.position['3B'] = self.b + self.ion2
+		self.position['4B'] = self.a + self.b + self.ion2
+		self.position['5B'] = self.c + self.ion2
+		self.position['6B'] = self.a + self.c + self.ion2
+		self.position['7B'] = self.b + self.c + self.ion2
+		self.position['8B'] = self.a + self.b + self.c + self.ion2
 
 	
-	def LatticePoints(self,R):
+	def lattice_points(self,R):
 		"""Generates a lattice with spacing twice the unit cell vectors, centred on (0,0,0)
 		returns a list of lattice sites across a grid of size 2R x 2R x 2R, which will encompass
 		the relevant sphere
@@ -88,205 +89,120 @@ class MonoclinicLattice:
 		
 		return vertices
 
-	def ShiftAndTrim(self,vertices,R,iNumber,iLetter,jNumber,jLetter):
+	def lattice_sphere(self,R,iNumber,iLetter,jNumber,jLetter):
 		"""
-		Given a lattice, it shifts of origin to be centred on the jth ion
-		Then return all latice points within R of the ith ion
+		Returns the lattice points of a lattice centred on the jth ion, for all ions within R of the ith ion
 		"""
-
-		#Shift vertices to be the lattice generated the from the jth position
-		vertices = vertices + self.Position[str(jNumber) + jLetter]
+		vertices = self.lattice_points(R)
+		#Shift vertices to be the lattice generated from the jth position
+		vertices = vertices + self.position[str(jNumber) + jLetter]
 		#Calculate distances from the ith atom to each other atom
-		distance = np.sqrt(np.sum(np.power(vertices - self.Position[str(iNumber) + iLetter],2),axis=1))
+		distance = np.sqrt(np.sum(np.power(vertices - self.position[str(iNumber) + iLetter],2),axis=1))
 		#only keep the locations of which are within a distance R from ion i
 		#I take the intersection with non-zero distances to avoid counting origin when ith and jth ions are equal
 		vertices = vertices[(distance < R) & (distance != 0.0)]
 		
 		return vertices
 
+	def lattice_sums(self,R):
+		#SI UNITS
+		mB=9.274*10**(-24) 
+		k=1.380*10**(-23)
+		NA=6.022*10**23 
+		mu0=4*pi*10**(-7) 			
+		factor = (mu0*mB**2)/(32*k*pi)/((10**(-10))**3)
 
-	
-		jLetter = 'A'
-		while True:
-			with open(jLetter+'MatrixValuesDyCl3New.csv', 'w') as csvfile:
-				writer = csv.writer(csvfile, lineterminator = '\n')
-				
-				verticesbase = self.LatticePoints(R)
-				
-				for jNumber in np.arange(1,9): 
-					
-					iVector = self.Position['1A']
+		A = {}
+		B = {}
 
-					vertices = self.ShiftAndTrim(verticesbase,R,1,'A',jNumber,jLetter)				
-					
-					x = vertices[:,0] - iVector[0]				
-					y = vertices[:,1] - iVector[1]
-					z = vertices[:,2] - iVector[2]
-					r = np.sqrt(np.sum(np.power(vertices - iVector,2),axis=1))
-					 
-					xx = sum(((r**2-3*x**2)/r**5))
-					yy = sum(((r**2-3*y**2)/r**5))
-					zz = sum(((r**2-3*z**2)/r**5))
-					xy = sum(((-3*x*y)/r**5))
-					xz = sum(((-3*x*z)/r**5))
-					yz = sum(((-3*y*z)/r**5)) 
-			                       
-					writer.writerow([xx,xy,xz])
-					writer.writerow([xy,yy,yz])
-					writer.writerow([xz,yz,zz])
-			if jLetter == 'A':
-				jLetter = 'B'
-			else:
-				break
+		for jNumber in np.arange(1,9):
 
-	def WriteLatticeSums(self,R):
-		jLetter = 'A'
-		while True:
-			with open(jLetter+'MatrixValuesDyCl3New.csv', 'w') as csvfile:
-				writer = csv.writer(csvfile, lineterminator = '\n')
-				
-				verticesbase = self.LatticePoints(R)
-				
-				for jNumber in np.arange(1,9): 
-					
-					iVector = self.Position['1A']
-
-					vertices = self.ShiftAndTrim(verticesbase,R,1,'A',jNumber,jLetter)				
-					
-					x = vertices[:,0] - iVector[0]				
-					y = vertices[:,1] - iVector[1]
-					z = vertices[:,2] - iVector[2]
-					r = np.sqrt(np.sum(np.power(vertices - iVector,2),axis=1))
-					 
-					xx = sum(((r**2-3*x**2)/r**5))
-					yy = sum(((r**2-3*y**2)/r**5))
-					zz = sum(((r**2-3*z**2)/r**5))
-					xy = sum(((-3*x*y)/r**5))
-					xz = sum(((-3*x*z)/r**5))
-					yz = sum(((-3*y*z)/r**5)) 
-			                       
-					writer.writerow([xx,xy,xz])
-					writer.writerow([xy,yy,yz])
-					writer.writerow([xz,yz,zz])
-			if jLetter == 'A':
-				jLetter = 'B'
-			else:
-				break
-
-
-	def CreateAMatrix(self,factor,demagnetisation):
-	    with open('AMatrixValuesDyCl3New.csv') as csvfile:
-	        spamreader = csv.reader(csvfile,quoting=csv.QUOTE_NONNUMERIC)
-	        self.A = {}
-	        n = 0
-	        m = 11    
-	        for row in spamreader:
-	            if n == 0:
-	                self.A[m] = np.zeros((3,3))
-	                self.A[m][0,0] = ((row[0] + demagnetisation[0,0])*self.gx*self.gx)*factor
-	                self.A[m][0,1] = ((row[1] + demagnetisation[0,1])*self.gx*self.gy)*factor
-	                self.A[m][0,2] = ((row[2] + demagnetisation[0,2])*self.gx*self.gz)*factor
-	                n += 1
-	            elif n == 1:
-	                self.A[m][1,0] = ((row[0] + demagnetisation[1,0])*self.gy*self.gx)*factor
-	                self.A[m][1,1] = ((row[1] + demagnetisation[1,1])*self.gy*self.gy)*factor
-	                self.A[m][1,2] = ((row[2] + demagnetisation[1,2])*self.gy*self.gz)*factor
-	                n += 1
-	            else:
-	                self.A[m][2,0] = ((row[0] + demagnetisation[2,0])*self.gz*self.gx)*factor
-	                self.A[m][2,1] = ((row[1] + demagnetisation[2,1])*self.gz*self.gy)*factor
-	                self.A[m][2,2] = ((row[2] + demagnetisation[2,2])*self.gz*self.gz)*factor
-	                n = 0
-	                m += 1
-
-
-	def CreateBMatrix(self,factor,demagnetisation):
-	    with open('BMatrixValuesDyCl3New.csv') as csvfile:
-	        spamreader = csv.reader(csvfile,quoting=csv.QUOTE_NONNUMERIC)
-	        self.B = {}
-	        n = 0
-	        m = 11    
-	        for row in spamreader:
-	            if n == 0:
-	                self.B[m] = np.zeros((3,3))
-	                self.B[m][0,0] = ((row[0] + demagnetisation[0,0])*self.gx*self.gx)*factor
-	                self.B[m][0,1] = ((row[1] + demagnetisation[0,1])*self.gx*self.gy)*factor
-	                self.B[m][0,2] = ((row[2] + demagnetisation[0,2])*self.gx*self.gz)*factor
-	                n += 1
-	            elif n == 1:
-	                self.B[m][1,0] = ((row[0] + demagnetisation[1,0])*self.gy*self.gx)*factor
-	                self.B[m][1,1] = ((row[1] + demagnetisation[1,1])*self.gy*self.gy)*factor
-	                self.B[m][1,2] = ((row[2] + demagnetisation[1,2])*self.gy*self.gz)*factor
-	                n += 1
-	            else:
-	                self.B[m][2,0] = ((row[0] + demagnetisation[2,0])*self.gz*self.gx)*factor
-	                self.B[m][2,1] = ((row[1] + demagnetisation[2,1])*self.gz*self.gy)*factor
-	                self.B[m][2,2] = ((row[2] + demagnetisation[2,2])*self.gz*self.gz)*factor
-	                n = 0
-	                m += 1
-
-	def set_L_matrices(self):
-		A = self.A
-		B = self.B
-		LA = {}
-		LA[1] = A[11]+A[12]+A[13]+A[14]+A[15]+A[16]+A[17]+A[18]
-		LA[2] = A[11]-A[12]-A[13]+A[14]-A[15]+A[16]+A[17]-A[18]
-		LA[3] = A[11]+A[12]-A[13]-A[14]+A[15]+A[16]-A[17]-A[18]
-		LA[4] = A[11]+A[12]+A[13]+A[14]-A[15]-A[16]-A[17]-A[18]
-		LA[5] = A[11]-A[12]+A[13]-A[14]+A[15]-A[16]+A[17]-A[18]
-		LA[6] = A[11]+A[12]-A[13]-A[14]-A[15]-A[16]+A[17]+A[18]
-		LA[7] = A[11]-A[12]-A[13]+A[14]+A[15]-A[16]-A[17]+A[18]
-		LA[8] = A[11]-A[12]+A[13]-A[14]-A[15]+A[16]-A[17]+A[18]
-		LB = {}
-		LB[1] = B[11]+B[12]+B[13]+B[14]+B[15]+B[16]+B[17]+B[18]
-		LB[2] = B[11]-B[12]-B[13]+B[14]-B[15]+B[16]+B[17]-B[18]
-		LB[3] = B[11]+B[12]-B[13]-B[14]+B[15]+B[16]-B[17]-B[18]
-		LB[4] = B[11]+B[12]+B[13]+B[14]-B[15]-B[16]-B[17]-B[18]
-		LB[5] = B[11]-B[12]+B[13]-B[14]+B[15]-B[16]+B[17]-B[18]
-		LB[6] = B[11]+B[12]-B[13]-B[14]-B[15]-B[16]+B[17]+B[18]
-		LB[7] = B[11]-B[12]-B[13]+B[14]+B[15]-B[16]-B[17]+B[18]
-		LB[8] = B[11]-B[12]+B[13]-B[14]-B[15]+B[16]-B[17]+B[18]
-		self.L = {}
-		for key in LA:
-			self.L[key] = LA[key] + LB[key]
-		for key in LA:
-			self.L[key+8] = LA[key] - LB[key]
-	
-
-
-	def CreateMatrices(self,factor,demagnetisation):
-		DyCl3.CreateAMatrix(factor,demagnetisation)
-		DyCl3.CreateBMatrix(factor,demagnetisation)
-		DyCl3.set_L_matrices()
-
-
-	def EigenVals(self):
-		eigvals = {}
-		for key in self.L:
-			eigvals[key] = np.linalg.eigvals(self.L[key])
-		with open('EigValsNew.csv', 'w') as csvfile:
-			writer = csv.writer(csvfile, lineterminator = '\n')  
-			for key in eigvals:                   
-				writer.writerow(eigvals[key])
-		# print(eigvals[1])
-
+			#obtain a sphere of lattice points of radius R centred on ion position 1A				
+			iVector = self.position['1A']				
+			vertices = self.lattice_sphere(R,1,'A',jNumber,'A')				
 			
+			#calculate the relative position of every lattice point
+			x = vertices[:,0] - iVector[0]				
+			y = vertices[:,1] - iVector[1]
+			z = vertices[:,2] - iVector[2]
+			r = np.sqrt(np.sum(np.power(vertices - iVector,2),axis=1))
+			
+			#calculate lattice sums
+			xx = sum(((r**2-3*x**2)/r**5))
+			yy = sum(((r**2-3*y**2)/r**5))
+			zz = sum(((r**2-3*z**2)/r**5))
+			xy = sum(((-3*x*y)/r**5))
+			xz = sum(((-3*x*z)/r**5))
+			yz = sum(((-3*y*z)/r**5)) 
+	                       
+			A[jNumber] = factor*np.array([[xx, xy, xz],[xy,yy,yz],[xz,yz,zz]])*self.g_grid
+
+		for jNumber in np.arange(1,9):
+
+			#obtain a sphere of lattice points of radius R centred on ion position 1B			
+			iVector = self.position['1A']				
+			vertices = self.lattice_sphere(R,1,'A',jNumber,'B')				
+			
+			#calculate the relative position of every lattice point
+			x = vertices[:,0] - iVector[0]				
+			y = vertices[:,1] - iVector[1]
+			z = vertices[:,2] - iVector[2]
+			r = np.sqrt(np.sum(np.power(vertices - iVector,2),axis=1))
+			
+			#calculate lattice sums
+			xx = sum(((r**2-3*x**2)/r**5))
+			yy = sum(((r**2-3*y**2)/r**5))
+			zz = sum(((r**2-3*z**2)/r**5))
+			xy = sum(((-3*x*y)/r**5))
+			xz = sum(((-3*x*z)/r**5))
+			yz = sum(((-3*y*z)/r**5)) 
+	                       
+			B[jNumber] = factor*np.array([[xx, xy, xz],[xy,yy,yz],[xz,yz,zz]])*self.g_grid
+
+		return A, B
+		# return A + demag, B + demag
+
+	def L_matrices(self, A, B):
+		LA = {}
+		LA[1] = A[1]+A[2]+A[3]+A[4]+A[5]+A[6]+A[7]+A[8]
+		LA[2] = A[1]-A[2]-A[3]+A[4]-A[5]+A[6]+A[7]-A[8]
+		LA[3] = A[1]+A[2]-A[3]-A[4]+A[5]+A[6]-A[7]-A[8]
+		LA[4] = A[1]+A[2]+A[3]+A[4]-A[5]-A[6]-A[7]-A[8]
+		LA[5] = A[1]-A[2]+A[3]-A[4]+A[5]-A[6]+A[7]-A[8]
+		LA[6] = A[1]+A[2]-A[3]-A[4]-A[5]-A[6]+A[7]+A[8]
+		LA[7] = A[1]-A[2]-A[3]+A[4]+A[5]-A[6]-A[7]+A[8]
+		LA[8] = A[1]-A[2]+A[3]-A[4]-A[5]+A[6]-A[7]+A[8]
+		LB = {}
+		LB[1] = B[1]+B[2]+B[3]+B[4]+B[5]+B[6]+B[7]+B[8]
+		LB[2] = B[1]-B[2]-B[3]+B[4]-B[5]+B[6]+B[7]-B[8]
+		LB[3] = B[1]+B[2]-B[3]-B[4]+B[5]+B[6]-B[7]-B[8]
+		LB[4] = B[1]+B[2]+B[3]+B[4]-B[5]-B[6]-B[7]-B[8]
+		LB[5] = B[1]-B[2]+B[3]-B[4]+B[5]-B[6]+B[7]-B[8]
+		LB[6] = B[1]+B[2]-B[3]-B[4]-B[5]-B[6]+B[7]+B[8]
+		LB[7] = B[1]-B[2]-B[3]+B[4]+B[5]-B[6]-B[7]+B[8]
+		LB[8] = B[1]-B[2]+B[3]-B[4]-B[5]+B[6]-B[7]+B[8]
+		L = {}
+		for key in LA:
+			L[key] = LA[key] + LB[key]
+		for key in LA:
+			L[key+8] = LA[key] - LB[key]
+		return L
+
+	def configuration_energies(self, R):
+		A,B = self.lattice_sums(R)
+		L = self.L_matrices(A,B)
+		eigvals = {}
+		for key in L:
+			eigvals[key] = np.linalg.eigvals(L[key])
+		return eigvals
+
 
 
 DyCl3 = MonoclinicLattice()
-DyCl3.Axes(9.61, 6.49, 7.87, 93.65*pi/180)
-DyCl3.gTensor(1.76, 1.76, 16.52, 157*pi/180)
-DyCl3.Ion1Position(0.25, 0.1521, 0.25)
-DyCl3.Ion2Position(0.75, 0.8479, 0.75)
-DyCl3.WriteLatticeSums(100)
-#SI UNITS
-mB=9.274*10**(-24) 
-k=1.380*10**(-23)
-NA=6.022*10**23 
-mu0=4*pi*10**(-7) 
-factor = (mu0*mB**2)/(k*8*4*pi)/((10**(-10))**3)
-demagnetisation = np.array([[-0.00106,0,0],[0,-0.00106,0],[0,0,-0.00106]])
-demagnetisation =  np.array([[0,0,0],[0,0,0],[0,0,0]])
+DyCl3.axes(9.61, 6.49, 7.87, 93.65*pi/180)
+DyCl3.g_tensor(1.76, 1.76, 16.52, 157*pi/180)
+DyCl3.ion1_position(0.25, 0.1521, 0.25)
+DyCl3.ion2_position(0.75, 0.8479, 0.75)
 
-DyCl3.CreateMatrices(factor,demagnetisation)
-DyCl3.EigenVals()
+eigvals = DyCl3.configuration_energies(100)
+print(eigvals)
